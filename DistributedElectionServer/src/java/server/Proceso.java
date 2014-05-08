e6ce0c0e2b074695cec0ba9880174840fdf00400
carlos1991 <carlosma_1991@hotmail.com>
2014-05-08 21:15:10 +0200

    REST en lugar de SOAP etc

--- DistributedElectionServer/src/java/server/Proceso.java
+++ DistributedElectionServer/src/java/server/Proceso.java
@@ -11,13 +11,16 @@ import java.util.logging.Logger;
  *
  * @author Carlos-p
  */
-public class Proceso implements Runnable {
+public class Proceso extends Thread {
     
     private final int NUM_PROCESOS = 6;
             
     private int id;
     private Estado estado;
     private int coordinador;
+    
+    // Primera IP -> procesos 1 y 2; Segunda IP -> procesos 3 y 4;...
+    private final static String [] IPS = {"127.0.0.1", "127.0.0.1", "127.0.0.1"};
     
     public Proceso (int id) {
         this.id = id;
@@ -28,10 +31,10 @@ public class Proceso implements Runnable {
     public void run() {
         while (estado == Estado.Corriendo) {
             try {
-                Thread.sleep((long)(Math.random()*500 + 500));
-                
-                //if (coordinador.computar() < 0) {
-                    eleccion();
+                Thread.sleep((long)(Math.random() * 500 + 500));
+                // int respuesta = IPS[coordinador] -> computar
+                //if (respuesta < 0) {
+                    this.eleccion();
                 //}
             } catch (InterruptedException ex) {
                 Logger.getLogger(Proceso.class.getName()).log(Level.SEVERE, null, ex);
@@ -45,7 +48,7 @@ public class Proceso implements Runnable {
     
     public void arrancar() {
         estado = Estado.Corriendo;
-        this.run();
+        this.start();
     }
     
     public void parar() {
@@ -57,6 +60,29 @@ public class Proceso implements Runnable {
     }
     
     public void eleccion() {
+        boolean tengoLaMayorId = true;
+        for (int j = id; j < NUM_PROCESOS; j++) {
+            // respuesta -> ok o computar al proceso j
+            //if (respuesta >= 0) {
+            //  tengoLaMayor = false;
+            //  exit;
+            //}
+        }
+        if (tengoLaMayorId) {
+            coordinador = id;
+            // pj.coordinador(id) para todo pj en (procesos activos)
+        } else {
+            // pj.eleccion() para todo pj > id
+            // esperar mensaje respuesta
+            //      si recible mensaje respuesta
+            //          esperar mensaje coordinador
+            //          si recibe mensaje coordinador(x)
+            //              pi.coordinador <- x
+            //          si no 
+            //              this.eleccion();
+            //      si no 
+            //          this.eleccion();
+        }
         
     }
     
--- DistributedElectionServer/src/java/server/Server.java
+++ DistributedElectionServer/src/java/server/Server.java
@@ -18,36 +18,8 @@ import javax.jws.WebService;
 @WebService(serviceName = "Server")
 public class Server {
     
-    private static Proceso [] procesos = new Proceso[2]; 
     
-    @WebMethod(operationName = "arrancaProceso")
-    public String arrancaProceso(@WebParam(name = "idProceso") int idProceso) {
-        if (procesos[idProceso % 2] == null) {
-            Proceso p;
-            Thread t = new Thread(p = new Proceso(idProceso));
-            procesos[idProceso % 2] = p;
-        } else {
-            procesos[idProceso % 2].arrancar();
-        } 
-        return "Arrancado el proceso " + idProceso;
-    }
-    
-    @WebMethod(operationName = "pararProceso")
-    public String pararProceso(@WebParam(name = "idProceso") int idProceso) {
-        procesos[idProceso % 2].parar();
-        return "Parado el proceso " + idProceso;
-    }
-    
-    @WebMethod(operationName = "computar")
-    public String computar(@WebParam(name = "idProceso") int idProceso) {
-        return "Hello";
-    }
-    
-    @WebMethod(operationName = "eleccion")
-    public String eleccion(@WebParam(name = "idProceso") int idProceso,
-                           @WebParam(name = "idCandidato") int idCandidato) {
-        return "Hello";
-    }
+
     
     
 }
